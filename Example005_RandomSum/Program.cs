5e09a43df95e64eb73f7fce6e004e907901f6ad3
Sergeevich <mrssn84@mail.ru>
2023-06-25 21:21:57 +0400

    Create Readme

--- Example005_RandomSum/Program.cs
+++ Example005_RandomSum/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = new Random().Next(1,10); // случайное дробное число от 1 до 9
+int numberA = new Random().Next(1,10); // случайное целое число от 1 до 9
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1,10); // случайное дробное число от 1 до 9
+int numberB = new Random().Next(1,10); // случайное целое число от 1 до 9
 Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
