e51557b79cc37004833989aa540f809024e039a4
Sergeevich <mrssn84@mail.ru>
2023-06-27 23:26:29 +0400

    HomeWork 2

---
+++ HW1.6(13)/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int number =  Convert.ToInt32(Console.ReadLine());
+string str = number.ToString();
+
+if (number / 100 > 1)
+{
+  Console.WriteLine($"Третья цифра числа {number} равна {str[2]}");
+}
+else
+{
+  Console.WriteLine($"У числа {number} третьей цифры нет");
+}
+
