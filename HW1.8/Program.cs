937a4fc531182b5c7f5f9297055bcf88f893ce7c
Sergeevich <mrssn84@mail.ru>
2023-07-01 14:26:48 +0400

    HW 3

--- HW1.8/Program.cs
+++ HW1.8/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+while (true)
+{
+  Console.WriteLine("Введите пятизначное число");
+  int num = Convert.ToInt32(Console.ReadLine());
+  if (num / 10000 >= 1 && num / 10000 <= 10)
+  {
+    bool result = Palindrom(num);
+    Console.WriteLine($"Число {num} - палиндром? - {result}");
+    return;
+  }
+  else
+  {
+    Console.WriteLine($"Вы ввели не пятизначное число. Попробуйте еще раз");
+  }
+}
+
+bool Palindrom(int num)
+{
+  int number1 = num % 10;
+  int number2 = num / 10 % 10;
+  int number4 = num / 1000 % 10;
+  int number5 = num / 10000 % 10;
+
+  bool res = false;
+  if (number1 == number5 && number2 == number4) res = true;
+  return res;
+}
+
---
+++ HW1.9/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите x первой точки");
+int aX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y первой точки");
+int aY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z первой точки");
+int aZ = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x второй точки");
+int bX = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y второй точки");
+int bY = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z второй точки");
+int bZ = Convert.ToInt32(Console.ReadLine());
+
+int a = aY - bY;
+int b = aX - bX;
+int c = aZ - bZ;
+
+
+double distance = Math.Sqrt(a * a + b * b + c * c);
+
+Console.WriteLine ($"Растояние между точками равно {distance}");
