d1caba4b61472fd6c57d8a1a0bbc3ad43819406f
Sergeevich <mrssn84@mail.ru>
2023-07-06 00:05:46 +0400

    HW 4

---
+++ HW1.12/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+Console.WriteLine("Задайте любое целое число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int getSumNumber(int num)
+{
+int sum = 0;
+  while(num > 0)
+  {
+   sum += num % 10;
+   num = num / 10;
+  }
+  return sum;
+}
+
+Console.WriteLine($"Сумма всех чисел числа {number} равна {getSumNumber(number)}");
---
+++ HW1.13/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+
+
+
+Console.WriteLine("Задайте начальное значение массива");
+int minimum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте конечное значение массива");
+int maximum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте количество элементов массива");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = generationArray(number, minimum, maximum);
+
+
+int[] generationArray(int num, int min, int max)
+{
+  int[] array = new int[num];
+  for (int i = 0; i < array.Length; i++)
+  {
+  array[i] = new Random().Next(min,max+1);
+  }
+  return array;
+}
+
+void PrintArray(int[] arr)
+{
+  for (int i = 0; i < arr.Length; i++)
+  {
+    Console.Write(arr[i]);
+    if(i < arr.Length-1)
+    {
+    Console.Write(", ");
+    }
+  }
+}
+
+
+PrintArray(arr);
